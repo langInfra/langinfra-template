599c968611ea8bf7ea71296f709e1a998fa1d99e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-01-06 12:16:50 +0600

    Create main.py

---
+++ src/langinfra_template/main.py
@@ -0,0 +1,23 @@
+import json
+from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
+from langinfra.__main__ import (
+    app as existing_app,
+    # main as existing_main,
+)  # Import the original app
+
+
+@existing_app.callback(invoke_without_command=True)
+def run():
+    components_path = Path(__file__).parent / "components"
+    os.environ["LANGINFRA_COMPONENTS_PATH"] = json.dumps([str(components_path)])
+    load_dotenv(find_dotenv())
+
+
+def main():
+    existing_app()  # Run the existing app
+
+
+if __name__ == "__main__":
+    main()
